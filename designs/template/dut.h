3f59378740426111b5b2867a936e7f9dd749508c
Azarenkov, Leonid <leonid.azarenkov@intel.com>
2022-11-28 12:44:16 -0800

    update designs/template

--- designs/template/dut.h
+++ designs/template/dut.h
@@ -9,33 +9,42 @@
  
 struct Dut : sc_module 
 {
+    typedef sc_uint<16> data_t;
+    
     sc_in<bool>         clk{"clk"};
     sc_in<bool>         rstn{"rstn"};
     
-    sc_signal<sc_uint<16>>  s{"s"};
+    sc_in<data_t>       inp{"inp"};
+    sc_out<data_t>      outp{"outp"};
+    
     
     SC_CTOR(Dut) 
     {
-	SC_CTHREAD(threadProc, clk.pos());
-	async_reset_signal_is(rstn, false);
+        SC_CTHREAD(threadProc, clk.pos());
+        async_reset_signal_is(rstn, false);
 
-	SC_METHOD(methodProc);
-	sensitive << s;  // Add all signal/ports read in method function here
+        SC_METHOD(methodProc);
+        sensitive << tmps;  // Add all signal/ports read in method function here
     }
+    
+    sc_signal<data_t>   tmps{"tmps"};
 
     void threadProc() 
     {
         // Reset signal/output port values here
+        tmps = 0;
         wait();
         
         while (true) {
             // Place sequential logic here
+            tmps = inp;
             wait();
         }
     }
 
     void methodProc() {
         // Place combinational logic here
+        outp = tmps.read() + data_t(1);
     }
 
 };
